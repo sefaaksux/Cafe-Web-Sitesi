15ea3ab552405f0e80270dbf64d90630eb0a95a4
sefaaksu <sefaaksusefa@gmail.com>
2024-03-30 22:34:42 +0300

    createdDeleteController

---
+++ Controllers/DeleteController.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using admin_panel.Models;
+using admin_panel.Data;
+using admin_panel.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace admin_panel.Controllers;
+
+public class DeleteController : Controller
+{
+    private readonly MyContext _context;
+
+    public DeleteController(MyContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> Index(int id)
+    {   
+        var product =await _context.urunler
+                    .Include(x => x.Tablo)
+                    .Include(x => x.Kategori)
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+        ViewBag.Tablolar = new SelectList(_context.tablolar.ToList(),"tabloId","tabloName");
+        ViewBag.Kategoriler = new SelectList(_context.kategoriler.ToList(),"kategoriId","kategoriName"); 
+        if (product == null)
+        {
+            return NotFound(); // Ürün bulunamazsa hata sayfasına yönlendirme yapabilirsiniz
+        }
+       
+       return View(product);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id)
+    {
+           var product = await _context.urunler.FindAsync(id);
+            
+            if(product == null)
+            {
+                return NotFound();
+            }
+                try
+                {                   
+                    _context.Remove(product);
+                    await _context.SaveChangesAsync();   
+                }
+                catch (System.Exception)
+                {              
+                    throw;
+                }
+            
+            
+            return RedirectToAction("AdminIndex","Home");
+    }
+
+}
